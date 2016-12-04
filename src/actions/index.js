b4d9c3480dd3335c902b109b9951bfa57e119c26
josephrstraub <josephrstraub@gmail.com>
2016-12-03 23:19:50 -0500

    two basic reducers and actions

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const dealCard = (card) => ({
+	type: 'CARD_DEAL',
+	card
+})
+
+export const removeCard = () => ({
+	type: 'CARD_REMOVE'
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from "redux"
+import deck from './deck'
 import playerHand from './player-hand'
 
-export default combineReducers({playerHand})
+export default combineReducers({
+	playerHand,
+	deck
+})
 
