e56ada718366ee3cc94c1ac885a41c59eccec0cd
josephrstraub <josephrstraub@gmail.com>
2016-12-04 12:14:22 -0500

    key for mapped values

--- src/components/PlayerChoices.js
+++ src/components/PlayerChoices.js
@@ -5,7 +5,9 @@ const choices = ["Split", "Double", "Hit", "Stand"]
 
 const PlayerChoices = ({ nextCard, dealCard }) => (
 	<div>
-		{choices.map(choice => <FloatingActionButton onClick={dealCard.bind(null, nextCard)}>{choice}</FloatingActionButton>)}
+		{choices.map((choice, index) => (
+			<FloatingActionButton key={index} onClick={dealCard.bind(null, nextCard)}>{choice}</FloatingActionButton>
+		))}
 	</div>
 )
 
