234c8da409128241f73655e3ea3a06e43886fbe1
josephrstraub <josephrstraub@gmail.com>
2016-12-04 13:57:38 -0500

    key for mapped values

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -4,7 +4,7 @@ import PlayingCard from './PlayingCard'
 
 const Hand = ({ hand }) => (
 	<div>
-		{hand.map(card => <PlayingCard value={card.value} suit={card.suit} />)}
+		{hand.map((card, index) => <PlayingCard key={index} value={card.value} suit={card.suit} />)}
 	</div>
 )
  
