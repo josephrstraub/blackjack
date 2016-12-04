cc0a2cc7ac07715b9b8c3b6cf621c6a487fec6ba
josephrstraub <josephrstraub@gmail.com>
2016-12-04 17:12:00 -0500

    changed grid to bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,25 @@ import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
+import '../grid.css'
+import { Grid, Row, Col } from 'react-bootstrap'
 import HandContainer from '../containers/HandContainer'
 import PlayerChoicesContainer from '../containers/PlayerChoicesContainer'
  
 const App = () => (
   <MuiThemeProvider>
-  	<div>
-	    <HandContainer />
-	    <PlayerChoicesContainer />
-	</div>
+  	<Grid>
+  		<Row>
+	    	<Col xs={6} xsOffset={3}>
+	    		<HandContainer />
+	    	</Col>
+	    </Row>
+	    <Row>
+	    	<Col xs={4} xsOffset={4}>
+	    		<PlayerChoicesContainer />
+	    	</Col>
+	    </Row>
+	</Grid>
   </MuiThemeProvider>
 )
  
