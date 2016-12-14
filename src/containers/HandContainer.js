9ca6f6172a54a879e6abb8ba67b2fc6b92e91221
josephrstraub <josephrstraub@gmail.com>
2016-12-14 14:15:18 -0500

    hidden card revealed at end, not beginning

--- src/containers/HandContainer.js
+++ src/containers/HandContainer.js
@@ -5,7 +5,7 @@ import Hand from '../components/Hand'
 const mapStateToProps = (state, ownProps) => ({
 	hand: ownProps.dealer ? getDealerHand(state) : getPlayerHand(state, ownProps),
 	isDealerHand: ownProps.dealer,
-	shouldAnimate: ownProps.dealer && getDealerHand(state)[0] && !getDealerHand(state)[0].isVisible
+	shouldAnimate: ownProps.dealer && getDealerHand(state)[0] && getDealerHand(state)[0].isVisible
 })
 
 export default connect(mapStateToProps)(Hand)
