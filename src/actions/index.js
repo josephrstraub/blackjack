72554709f19f47591989a8312582bb9af4dcab05
josephrstraub <josephrstraub@gmail.com>
2016-12-17 21:22:59 -0500

    bankroll changes incrementally and rapidly

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,20 @@ import _ from 'lodash'
 
 export const makeNewGame = () => ({ type: 'NEW_GAME' })
 export const changeWagerSize = (size) => ({ type: 'WAGER_SIZE_CHANGE', size: size || 50 })
-const changePlayerBankroll = (amount) => ({ type: 'PLAYER_BANKROLL_CHANGE', amount })
+const changePlayerBankroll = (amount) => (dispatch, getState) => { 
+	let increment = amount / Math.abs(amount) * 25
+	let i = 0
+	let bankrollInterval = setInterval(
+		() => {
+			dispatch({ type: 'PLAYER_BANKROLL_CHANGE', amount: increment })
+			i += increment
+			if (i === amount) {
+				clearInterval(bankrollInterval)
+			}
+		},
+		.5
+	)
+}
 export const split = () => ({ type: 'SPLIT' })
 export const toggleAutoDeal = () => ({ type: 'AUTO_DEAL_TOGGLE' })
 
