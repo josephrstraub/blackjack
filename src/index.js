ac6eee3edd6ed60b6f959653d0b9f851d09d1838
josephrstraub <josephrstraub@gmail.com>
2016-12-20 14:06:14 -0500

    switched out logger in favor of redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
 import rootReducer from './reducers'
 import App from './components/App'
 import './index.css'
 
-const loggerMiddleware = createLogger()
-
-let store = createStore(
-	rootReducer,
-	applyMiddleware(loggerMiddleware, thunk)
-)
+let store = createStore(rootReducer, composeWithDevTools(
+	applyMiddleware(thunk)
+))
 
 ReactDOM.render(
 	<Provider store={store}>
