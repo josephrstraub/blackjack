af5f42db693767b0afb3ac1f02aac3212a032d8a
josephrstraub <josephrstraub@gmail.com>
2017-01-05 13:45:42 -0500

    added test case

--- src/selectors/selectors.test.js
+++ src/selectors/selectors.test.js
@@ -1,16 +1,25 @@
 import { getScore } from './index'
 
-const cards = [
+const cards1 = [
 	{ name: "6", value: 6, suit: "hearts" },
 	{ name: "ace", value: 11, suit: "diamonds" },
 	{ name: "6", value: 6, suit: "spades" }
 ]
+const cards2 = [
+	{ name: "4", value: 4, suit: "hearts" },
+	{ name: "ace", value: 11, suit: "spades" },
+	{ name: "ace", value: 11, suit: "hearts" },
+	{ name: "king", value: 10, suit: "spades" }
+]
 
 describe('selectors', () => {
 	it('should return 13', () => {
-		expect(getScore(cards)).toEqual(13)
+		expect(getScore(cards1)).toEqual(13)
 	})
 	it('should return 17', () => {
-		expect(getScore(cards.slice(0, 2))).toEqual(17)
+		expect(getScore(cards1.slice(0, 2))).toEqual(17)
+	})	
+	it('should return 16', () => {
+		expect(getScore(cards2)).toEqual(16)
 	})
 })
