56a11a845653475d4a8602a85365518ce56f5950
josephrstraub <josephrstraub@gmail.com>
2017-01-05 13:08:42 -0500

    nav close button only visible when needed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
@@ -6,26 +6,47 @@ import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
-const RightMenu = ({ makeNewGame }) => (
+const RightMenu = ({ menuIsOpen, handleClick, handleRequestChange }) => (
   <IconMenu
-    iconButtonElement={
-      <IconButton><MoreVertIcon /></IconButton>
-    }
+    iconButtonElement={ <IconButton><MoreVertIcon /></IconButton> }
+    open={menuIsOpen}
     targetOrigin={{horizontal: 'right', vertical: 'top'}}
     anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-    onItemTouchTap={makeNewGame}
+    onItemTouchTap={handleClick}
+    onRequestChange={handleRequestChange}
   >
     <MenuItem primaryText="Themes" />
     <MenuItem primaryText="Start Over" />
   </IconMenu>
 )
 
-const Header = (props) => (
-	<AppBar
-      id="header"
-    	title="BlackJack"
-    	iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    	iconElementRight={<RightMenu {...props} />} />        
-)
+class Header extends Component {
+  constructor() {
+    super()
+    this.state = { navCloseIsVisible: false, menuIsOpen: false }
+  }
+  handleClick(event, child) {
+    this.setState({menuIsOpen: !this.state.menuIsOpen})
+    this.setState({navCloseIsVisible: !this.state.navCloseIsVisible})
+    if (event.target.innerText === "Start Over") {
+      this.props.makeNewGame()
+    }
+  }
+  handleRequestChange(open, reason) {
+    this.setState({menuIsOpen: !this.state.menuIsOpen})
+    this.setState({navCloseIsVisible: !this.state.navCloseIsVisible})
+  }  
+	render() {
+    return (
+      <AppBar
+        id="header"
+      	title="BlackJack"
+        showMenuIconButton={this.state.navCloseIsVisible}
+      	iconElementLeft={ this.state.navCloseIsVisible ? <IconButton><NavigationClose /></IconButton> : null }
+      	iconElementRight={ <RightMenu menuIsOpen={this.state.menuIsOpen} handleClick={this.handleClick.bind(this)} handleRequestChange={this.handleRequestChange.bind(this)} /> } />
+    ) 
+  }     
+}
+
 
 export default Header
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -3,11 +3,7 @@ import { makeNewGame} from '../actions'
 import Header from '../components/Header'
 
 const mapDispatchToProps = (dispatch) => ({
-	makeNewGame: (event, child) => {
-		if (event.target.innerText === "Start Over") {
-			dispatch(makeNewGame())
-		}
-	}
+	makeNewGame: () => dispatch(makeNewGame())
 })
 
 export default connect(
