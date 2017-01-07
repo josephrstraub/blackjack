98d1a7a28851a581399b5c7f021e0d07308b5082
josephrstraub <josephrstraub@gmail.com>
2017-01-07 16:04:04 -0500

    request full screen api

--- src/index.js
+++ src/index.js
@@ -8,34 +8,20 @@ import rootReducer from './reducers'
 import App from './components/App'
 import './index.css'
 
-function hideAddressBar()
-{
-  if(!window.location.hash)
-  {
-      if(document.height < window.outerHeight)
-      {
-          document.body.style.height = (window.outerHeight + 50) + 'px'
-      }
-
-      setTimeout( function(){ window.scrollTo(0, 1); }, 50 )
-  }
-}
-
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } )
-window.addEventListener("orientationchange", hideAddressBar )
+window.addEventListener("load", () => document.documentElement.webkitRequestFullscreen() )
 
 const timeoutScheduler = store => next => action => {
   if (!action.meta || !action.meta.delay) {
-    return next(action)
+	return next(action)
   }
 
   let p = new Promise((resolve, reject) => {
 	  let timeoutId = setTimeout(
-	    () => {
-	    	next(action)
-	    	resolve()
-	    },
-	    action.meta.delay
+		() => {
+			next(action)
+			resolve()
+		},
+		action.meta.delay
 	  )
 	  return timeoutId
   })
@@ -49,7 +35,7 @@ let store = createStore(rootReducer, composeWithDevTools(
 
 ReactDOM.render(
 	<Provider store={store}>
-  		<App />
-  	</Provider>,
-  	document.getElementById('root')
+		<App />
+	</Provider>,
+	document.getElementById('root')
 )
